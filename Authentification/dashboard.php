6986605fe4e97d5b0a95b07e4a972a3faf10f345
FaissalMarzouki <fayssalmarzoki555@gmail.com>
2023-12-30 13:37:10 +0100

    index page

--- Authentification/dashboard.php
+++ Authentification/dashboard.php
@@ -14,18 +14,19 @@ if (!isset($_SESSION['user_id'])) {
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Dashboard</title>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css">
+
+    <title>Dashboard</title>
 </head>
 <body>
     <div class="container my-5">
         <h2>Welcome, <?php echo $_SESSION['username']; ?>!</h2>
-        <a class="btn btn-danger" href="/PHPPROJECT/Authentification/logout.php">Logout</a>
-        <a class="btn btn-primary" href="/PHPPROJECT/CrudDepartement/index.php">Go to Department Page</a>
-        <a class="btn btn-primary" href="/PHPPROJECT/CrudFiliere/indexfiliere.php">Go to filiere Page</a>
-        <a class="btn btn-primary" href="/PHPPROJECT/CrudEnseignant/indexenseignant.php">Go to Enseignant Page</a>
-        <a class="btn btn-primary" href="/PHPPROJECT/CrudEtudiant/indexetudiant.php">Go to Etudiant Page</a>
-        <a class="btn btn-primary" href="/PHPPROJECT/CrudMatiere/indexmatiere.php">Go to Matiere Page</a>
+        <a class="btn btn-danger" href="/PHPPROJECT/Authentification/logout.php">Logout</a><br><br>
+        <a class="btn btn-primary" href="/PHPPROJECT/CrudDepartement/index.php">Go to Department Page</a><br><br>
+        <a class="btn btn-primary" href="/PHPPROJECT/CrudFiliere/indexfiliere.php">Go to filiere Page</a><br><br>
+        <a class="btn btn-primary" href="/PHPPROJECT/CrudEnseignant/indexenseignant.php">Go to Enseignant Page</a><br><br>
+        <a class="btn btn-primary" href="/PHPPROJECT/CrudEtudiant/indexetudiant.php">Go to Etudiant Page</a><br><br>
+        <a class="btn btn-primary" href="/PHPPROJECT/CrudMatiere/indexmatiere.php">Go to Matiere Page</a><br><br>
         <a class="btn btn-primary" href="/PHPPROJECT/CrudSalle/indexsalle.php">Go to Salle Page</a>
     </div>
 </body>
