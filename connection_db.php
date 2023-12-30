73253e8c530663783de49172dbf4f7f549581c37
FaissalMarzouki <fayssalmarzoki555@gmail.com>
2023-12-30 15:15:15 +0100

    add connection file

---
+++ connection_db.php
@@ -0,0 +1,10 @@
+<?php
+try {
+    $connection = new PDO("mysql:host=localhost;dbname=gestion de scolarite", 'fayssal', '1447');
+    $connection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch (PDOException $e) {
+    die('Erreur : ' . $e->getMessage());
+}
+
+
+?>
